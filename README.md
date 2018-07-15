7f6addafe2fc49b222d6723276a9313b888a8e55
TANIGUCHI Masaya <mew@cat.ovh>
2018-07-15 11:22:26 +0900

    Support OpenCV

--- README.md
+++ README.md
@@ -2,17 +2,27 @@
 
 [![Clojars Project](https://img.shields.io/clojars/v/clj-imshow.svg)](https://clojars.org/clj-imshow)
 
-A implementation of OpenCV's imshow. We supports only JavaCV and JavaCPP Presets. In 2018 `imshow` and `waitKey` doesn't work on macOS, you can use this wrapper.
+A implementation of OpenCV's imshow. This library supports OpenCV and JavaCV/JavaCPP-Presets. In 2018 `imshow` and `waitKey` doesn't work on macOS, you can use this wrapper.
 
 ## Usage
 
-```clojure
-(import [org.bytedeco.javacpp.opencv_imgcodecs])
-(require '[clj-imshow.core :as cvx])
+- OpenCV
+    ```clojure
+    (import [org.opencv.imgcodecs Imgcodecs])
+    (require '[clj-imshow.core :as cvx])
+    
+    (cvx/imshow "lena" (Imgcodecs/imread "lena.png"))
+    (cvx/imshow (Imgcodecs/imread "lena.png")) ;; When you give no title, title is "Untitled"
+    ```
 
-(cvx/imshow "lena" (opencv_imgcodecs/imread "lena.png"))
-(cvx/imshow (opencv_imgcodecs/imread "lena.png")) ;; When you give no title, title is "Untitled"
-```
+- JavaCV/JAVACPP-Presets
+    ```clojure
+    (import [org.bytedeco.javacpp.opencv_imgcodecs])
+    (require '[clj-imshow.core :as cvx])
+    
+    (cvx/imshow "lena" (opencv_imgcodecs/imread "lena.png"))
+    (cvx/imshow (opencv_imgcodecs/imread "lena.png")) ;; When you give no title, title is "Untitled"
+    ```
 
 ## License
 
--- src/clj_imshow/core.clj
+++ src/clj_imshow/core.clj
@@ -1,5 +1,7 @@
 (ns clj-imshow.core
   (:import [org.bytedeco.javacpp opencv_imgcodecs]
+           [org.opencv.core Mat MatOfByte]
+           [org.opencv.imgcodecs Imgcodecs]
            [java.io ByteArrayInputStream]
            [javax.imageio ImageIO])
   (:use seesaw.core))
@@ -7,16 +9,26 @@
 (def imlabel (label))
 (def imframe (frame :content imlabel :on-close :dispose))
 
+(defn- toBufferedImage [image]
+  (if (instance? Mat image)
+    ;; OpenCV
+    (let [mob (MatOfByte.)]
+       (Imgcodecs/imencode ".png" image mob)
+       (-> mob .toArray ByteArrayInputStream. ImageIO/read))
+    ;; JavaCV
+    (let [ba (byte-array (* (.channels image) (.cols image) (.rows image)))]
+      (opencv_imgcodecs/imencode ".png" image ba)
+      (-> ba ByteArrayInputStream. ImageIO/read))))
+
 (defn imshow
   ([image]
    (imshow "Untitled" image))
   ([title image]
-   (let [ba (byte-array (* (.channels image) (.cols image) (.rows image)))]
-     (opencv_imgcodecs/imencode ".png" image ba)
-     (let [bi (-> ba ByteArrayInputStream. ImageIO/read)]
-       (if (.isShowing imframe)
-         (do (.setTitle imframe title)
-             (.setIcon imlabel (icon bi)))
-         (do (.setIcon imlabel (icon bi))
-             (.setTitle imframe title)
-             (invoke-later (-> imframe pack! show!))))))))
+   (let [bi (toBufferedImage image)]
+     (if (.isShowing imframe)
+       (do (.setTitle imframe title)
+           (.setIcon imlabel (icon bi)))
+       (do (.setIcon imlabel (icon bi))
+           (.setTitle imframe title)
+           (invoke-later (-> imframe pack! show!)))))))
+  
